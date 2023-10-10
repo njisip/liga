911d467822817c5230811f86b0c44f116b97f5f1
njisip <ndisip@up.edu.ph>
2023-10-10 14:45:05 +0800

    feat(landing): add landing page background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import NavBar from '@/components/NavBar'
+import styles from './Home.module.css'
 
 /** The component for the landing page. */
 export default async function Home() {
-	return <NavBar />
+	return (
+		<ContainerBackground>
+			<NavBar />
+		</ContainerBackground>
+	)
+}
+
+function ContainerBackground({ children }: { children: React.ReactNode }) {
+	return (
+		<div className={styles.mainBackground}>
+			<div className={styles.backgroundPattern}>
+				<div className={styles.backgroundMesh}>{children}</div>
+			</div>
+		</div>
+	)
 }

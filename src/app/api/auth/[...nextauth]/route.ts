d1fc32b609aa8f662fd1358020912e448918d7dd
njisip <ndisip@up.edu.ph>
2023-10-15 16:06:53 +0800

    feat(auth): add credentials provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from '@/db'
 import { PrismaAdapter } from '@auth/prisma-adapter'
+import { compare } from 'bcrypt'
 import NextAuth, { AuthOptions } from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: AuthOptions = {
@@ -15,6 +17,30 @@ export const authOptions: AuthOptions = {
 
 	// Configure one or more authentication providers
 	providers: [
+		Credentials({
+			name: 'Email',
+			credentials: {
+				email: { type: 'email', label: 'Email' },
+				password: { type: 'password', label: 'Password' },
+			},
+			async authorize(credentials) {
+				// Check credentials
+				if (!credentials?.email || !credentials?.password) return null
+
+				// Find user
+				var user = await prisma.user.findUnique({
+					where: { email: credentials.email },
+				})
+
+				// Compare password
+				if (!user?.password) return null
+				const valid = await compare(credentials.password, user.password)
+				if (valid) return user
+
+				return null
+			},
+		}),
+
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID!,
 			clientSecret: process.env.GOOGLE_SECRET!,
